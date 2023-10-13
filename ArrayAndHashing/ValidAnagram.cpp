6bba4de2761e73a3ac71d37e07812c8e027c1328
ethanawu <ethanwu9@gmail.com>
2023-10-13 16:36:07 -0700

    new github folder

---
+++ ArrayAndHashing/ValidAnagram.cpp
@@ -0,0 +1,27 @@
+
+using namespace std;
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm> 
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        if(s == t)
+            return true;
+        
+        return false;
+    }
+};
+
+/*
+    Time Complexity O(1)
+    Space Complexity O(1)
+*/
+
+//testing
+    
