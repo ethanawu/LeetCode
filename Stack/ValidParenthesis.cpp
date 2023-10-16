6a756575b9bd0fd812b3bf4454966211c2818b81
ethanawu <ethanwu9@gmail.com>
2023-10-15 21:20:06 -0700

    updated function

--- Stack/ValidParenthesis.cpp
+++ Stack/ValidParenthesis.cpp
@@ -1,43 +1,29 @@
-/*
-    Given s w/ '(, ), {, }, [, ]', determine if valid
-    Ex. s = "()[]{}" -> true, s = "(]" -> false
-
-    Stack of opens, check for matching closes & validity
-
-    Time: O(n)
-    Space: O(n)
-*/
 using namespace std;
-#include <iostream>
+
 #include <stack>
-#include <unordered_map>
+#include <iostream>
+#include <string>
+
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> open;
-        unordered_map<char, char> parens = {
-            {')', '('},
-            {']', '['},
-            {'}', '{'},
-        };
-        
-        for (const auto& c : s) {
-            if (parens.find(c) != parens.end()) {
-                // if input starts with a closing bracket.
-                if (open.empty()) {
+        stack<char> stack; //char stack
+        for(auto i : s) { //for each letter in string
+            if(i == '('  || i == '[' || i == '{') //if i is a opening bracket push
+                stack.push(i);
+            else {
+                if(stack.empty() || (stack.top() == '(' && i != ')') || (stack.top() == '{' && i != '}') || (stack.top() == '[' && i != ']'))
                     return false;
-                }
-
-                if (open.top() != parens[c]) {
-                    return false;
-                }
-
-                open.pop();
-            } else {
-                open.push(c);
+                stack.pop();  
             }
+                  
         }
+       
+        return stack.empty();
         
-        return open.empty();
     }
 };
+/*
+    Time: O(n)
+    Space: O(n)
+*/
